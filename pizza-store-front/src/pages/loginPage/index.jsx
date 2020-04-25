88a69e3f442d081117a0e03bfed25d8576188cfc
shehab20089 <xxxegygamerxxx@gmail.com>
2020-04-25 21:08:39 +0200

    making auth pages ui

--- pizza-store-front/src/pages/loginPage/index.jsx
+++ pizza-store-front/src/pages/loginPage/index.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import AuthForm from "../../components/authForm/index";
 
 const loginPage = () => {
-  return <div>please login</div>;
+  return (
+    <div>
+      <AuthForm title="Login">
+        <div className="input-group">
+          <label htmlFor="email">Email:</label>
+          <div className="frm-input">
+            <input type="email" id="email" placeholder="Enter Your Email" />
+          </div>
+        </div>
+        <div className="input-group">
+          <label htmlFor="pass">Password:</label>
+          <div className="frm-input">
+            <input
+              type="password"
+              id="pass"
+              placeholder="Enter Your password"
+            />
+          </div>
+        </div>
+      </AuthForm>
+    </div>
+  );
 };
 
 export default loginPage;
--- pizza-store-front/src/pages/registerPage/index.jsx
+++ pizza-store-front/src/pages/registerPage/index.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthForm from "../../components/authForm/index";
 
 const RegisterPage = () => {
-  return <div>please register your page</div>;
+  const [role, setrole] = useState("user");
+  let changeActive = val => {
+    setrole(val);
+  };
+
+  return (
+    <div>
+      <AuthForm title="Register">
+        <div className="input-group">
+          <label htmlFor="email">Email:</label>
+          <div className="frm-input">
+            <input type="email" id="email" placeholder="Enter Your Email" />
+          </div>
+        </div>
+        <div className="input-group">
+          <label htmlFor="Name">Name:</label>
+          <div className="frm-input">
+            <input type="text" id="Name" placeholder="Enter Your Name" />
+          </div>
+        </div>
+        <div className="input-group">
+          <label htmlFor="Address">Address:</label>
+          <div className="frm-input">
+            <input type="text" id="Address" placeholder="Enter Your Address" />
+          </div>
+        </div>
+        <div className="input-group">
+          <label>Role:</label>
+          <div className="role">
+            <div
+              className={`role-btn ${role == "user" ? "active" : ""}`}
+              onClick={() => changeActive("user")}
+            >
+              user
+            </div>
+            <div
+              className={`role-btn ${role == "admin" ? "active" : ""}`}
+              onClick={() => changeActive("admin")}
+            >
+              admin
+            </div>
+          </div>
+          {role == "admin" ? (
+            <div className="frm-input">
+              <input
+                type="admin"
+                placeholder="Enter Your admin code (admin code is 'admin')"
+              />
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="input-group">
+          <label htmlFor="pass">Password:</label>
+          <div className="frm-input">
+            <input
+              type="password"
+              id="pass"
+              placeholder="Enter Your password"
+            />
+          </div>
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="Cpass">Password:</label>
+          <div className="frm-input">
+            <input
+              type="password"
+              id="Cpass"
+              placeholder="please confirm your password"
+            />
+          </div>
+        </div>
+      </AuthForm>
+    </div>
+  );
 };
 
 export default RegisterPage;
